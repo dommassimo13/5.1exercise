b065032d1dcdd904f3ac1972679c22ba2d9ce4b0
dominic massimo <dominicmassimo@dominics-iMac.local>
2017-04-13 19:47:40 -0400

    Initial commit

---
+++ src/Math2.java
@@ -0,0 +1,16 @@
+/**
+ * Created by dominicmassimo on 4/13/17.
+ */
+public class Math2 {
+    public static void main(String [] args) {
+        System.out.println("Math.pow(7, 9) returns " + Math.pow(7, 9));
+        System.out.println("Math.sqrt(36) returns " + Math.sqrt(36));
+        System.out.println("Math.abs(-9) returns " + Math.abs(-9));
+        System.out.println("Math.random() returns " + Math.random());
+        System.out.println("Math.max(4, 6) returns " + Math.max(4, 6));
+        System.out.println("Math.min(1, 5) returns " + Math.min(1, 5));
+        System.out.println("Math.round(5.72) returns " + Math.round(5.72));
+        System.out.println("Math.ceil(8.37) returns " + Math.ceil(8.37));
+        System.out.println("Math.floor(4.75) returns " + Math.floor(4.75));
+    }
+}
